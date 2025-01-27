bad75e1bfa58e336e1dab9ab10549d5081b7d2e9
xiangqionglou <xiangqionglou@163.com>
2025-01-27 03:36:57 +0000

    marscode ai test

---
+++ src/main/java/com/xiangql/LuckMain.java
@@ -0,0 +1,11 @@
+package com.xiangql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuckMain {
+    public static void main(String[] args) {
+        SpringApplication.run(LuckMain.class, args);
+    }
+}
